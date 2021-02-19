c2ef6062a685fe9cb98afd65581763c5ade1e24e
Richard W Brown <70118501+rbrown540@users.noreply.github.com>
2021-02-19 10:41:51 -0500

    Add files via upload

---
+++ group_schedule.py
@@ -0,0 +1,44 @@
+{
+"Spin Cycle with Jesse":
+    {
+    "Monday": [{"0515": "30k 'Hello Week'"
+            }],
+    "Tuesday": [{"0915": "Hills and Core"
+            }],
+    "Thursday": [{"0915": "Core and Hills"
+            }],
+    "Friday": [{"0515": "500 cal Friday"
+           }]
+    },
+"Rock Solid":
+    {
+    "Monday": [{"0615": "Get up Get ready"
+        }],
+    "Tuesday": [{"0615": "Four for your Core"
+            }],
+    "Wednesday": [{"0615": "Mike Mike Mike, guess what day it is!"
+            }],
+    "Thursday": [{"0615": "Friday Junior"
+        }],
+    "Friday": [{"0615": "IT'S FINALLY OVER"
+        }]
+    },
+"Yoga Chill":
+    {
+    "Monday": [{"1015": "Shhh "
+            }],
+    "Tuesday": [{"1015": "More Shhh"
+            }],
+    "Thursday": [{"1015": "So, Much, Shhh"
+            }],
+    "Friday": [{"1015": " zzz "
+           }]
+    },
+"More Jesse Spin":
+    {
+    "Saturday": [{"0000": "You'll be okay!!"
+        }],
+    "Sunday": [{"0000": "What? Are you seriously NOT working out right now?"
+            }]
+    }
+}
